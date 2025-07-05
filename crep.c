d069fb729560ea746d1faedd292b7f09be2b1414
amin kafri <aminkafrinewgen@gmail.com>
2025-07-05 10:23:33 +0800

    add utility function for reading file and printing it's content

--- crep.c
+++ crep.c
@@ -1,8 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-#define min_return_defer(value) do { result = (value); goto defer } while(0); \
+
+#define MIN_ASSERT assert
+#define MIN_FREE free
+#define min_return_defer(value) do { result = (value); goto defer; } while(0); \
+
+typedef enum {
+	MIN_LOG,
+	MIN_WARNING,
+	MIN_ERROR,
+} Min_Log_Level;
+
+void min_log(Min_Log_Level log_level, const char *fmt, ...)
+{
+	switch (log_level) {
+	case MIN_LOG:
+		fprintf(stderr, "[LOG] ");
+		break;
+	case MIN_WARNING:
+		fprintf(stderr, "[WARNING] ");
+		break;
+	case MIN_ERROR:
+		fprintf(stderr, "[ERROR] ");
+		break;
+	default:
+		MIN_ASSERT(0 && "unreachable");
+	}
+
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fprintf(stderr, "\n");
+}
 
 typedef struct {
 	char *items;
@@ -15,18 +51,16 @@ void min_sb_append(Min_String_Builder *sb, char *item)
 	
 }
 
-int min_read_file(const char *path, Min_String_Builder *sb)
+bool min_read_file(const char *path, Min_String_Builder *sb)
 {
+	bool result = true;
 	size_t buf_size = 32 * 1024;
 	char *buf = malloc(buf_size * sizeof(char));
-	if (buf == NULL) {
-		perror("crep");
-		return 0;
-	}
+	MIN_ASSERT(buf != NULL && "Not enough memory");
 	FILE *file = fopen(path, "r");
     if (file == NULL) {
-		perror("crep");
-		return 0;
+		min_log(MIN_ERROR, "Failed to open file path %s: %s", path, strerror(errno));
+		min_return_defer(false);
 	}
 
 	size_t n = fread(buf, 1, buf_size, file); 
@@ -35,10 +69,14 @@ int min_read_file(const char *path, Min_String_Builder *sb)
 		n = fread(buf, 1, buf_size, file); 
 	}
 	if (ferror(file)) {
-		
+		min_log(MIN_ERROR, "Failed to read file %s: %s", path, strerror(errno));
+		min_return_defer(false);
 	}
 
-	fclose(file);
+ defer:
+	MIN_FREE(buf);
+	if (file) fclose(file);
+	return result;
 }
 
 int main(void)
@@ -46,8 +84,7 @@ int main(void)
 	const char *path = "sample-1.txt";
 
     Min_String_Builder buf = {0};
-	min_read_file(path, &buf);
+	if (!min_read_file(path, &buf)) return 1;
 
-	free(buf.items);
 	return 0;
 }

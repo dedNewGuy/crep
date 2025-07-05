0d7d4a1b73bd84bd38b5b2b19c1db72d6e1d7152
amin kafri <aminkafrinewgen@gmail.com>
2025-07-05 22:13:53 +0800

    print number of column as well

--- crep.c
+++ crep.c
@@ -11,27 +11,32 @@ int main(void)
 	min_log(MIN_LOG, "Count: %d, Capacity: %d", sb.count, sb.capacity);
 
 	min_log(MIN_LOG, "Starting search...");
-	int at_line = 0;
+
 	char *input = "is";
 	size_t idx = 0;
+	int at_line = 0;
+	int at_col = 0;
+
 	size_t char_at = 0;
 	while (char_at < sb.count) {
 		char c = sb.items[char_at];
 		// min_log(MIN_LOG, "Comparing %c & %c", c, input[idx]);
 		if (c == '\n') {
 			at_line = at_line + 1;
+			at_col = 0;
 			char_at++;
 			continue;
 		}
 		if (c == input[idx]) {
 			if (idx == strlen(input) - 1) {
-				fprintf(stderr, "%s:%d::\n", path, at_line + 1);
+				fprintf(stderr, "%s:%d:%d:note\n", path, at_line + 1, at_col);
 				idx = 0;
 			} else idx++;
 		} else if (idx != 0) {
 			idx = 0;
 			continue;
 		}
+		at_col++;
 		char_at++;
 	}
 	min_free_sb(sb);

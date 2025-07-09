a6fdee21af15a639513b124d69747e6b25017112
amin kafri <aminkafrinewgen@gmail.com>
2025-07-09 17:11:57 +0800

    make it command line tools

--- crep.c
+++ crep.c
@@ -21,6 +21,8 @@ void get_line_content(Min_String_Builder sb, Text_Pointer tp, char buf[])
 	// Later maybe need to add some sort of data structure to keep already read line
 	// Hash table? for now it's overkill I guess. Want to focus on implementing
 	// string matching algorithm first
+
+	// [NOTE] ONLY USED BY Naive implementation
 	size_t start = tp.start_line;
 	size_t lc_idx = 0;
 	while (sb.items[start] != '\n' && start < sb.count) {
@@ -30,8 +32,11 @@ void get_line_content(Min_String_Builder sb, Text_Pointer tp, char buf[])
 	buf[lc_idx] = '\0';
 }
 
+// NAIVE CAUSE SEGFAULT WHEN READING MULTIPLE FILES. IM NOT BOTHERED TO DEBUG THIS.
+// Prolly will if it matters. Main implementation will use DFA anyway.
 void naive_string_matching(Min_String_Builder sb, Text_Pointer *tp, const char *input)
 {
+	tp->at_line = 0;	
 	size_t in_len = strlen(input);
 	int idx = 0;
 	int char_at = 0;
@@ -87,14 +92,10 @@ void construct_dfa(const char *pattern, size_t n_possible_input, size_t n_state,
 
 }
 
-void dfa_string_matcher(const char *pattern, Min_String_Builder sb, Text_Pointer *tp)
+void dfa_string_matcher(size_t pat_len, size_t n_possible_input, int (*dfa_table)[n_possible_input], Min_String_Builder sb, Text_Pointer *tp)
 {
-	size_t ascii_count = 127;
-	size_t pat_len = strlen(pattern);
-	size_t n_state = pat_len + 1;
-	int dfa_table[n_state][ascii_count];
-	construct_dfa(pattern, ascii_count, n_state, dfa_table);
-
+	tp->at_line = 1;
+	
 	size_t state = 0;
 	size_t buf_len = 2 * 1024;
 	char buf[buf_len];
@@ -127,24 +128,31 @@ void dfa_string_matcher(const char *pattern, Min_String_Builder sb, Text_Pointer
 	}
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-	// TODO: Make it usable as command line tool for later
+	if (argc < 3) {
+		fprintf(stderr, "Usage: crep <pattern> <file...>\n");
+		return 1;
+	}
+
+	const char *pattern = argv[1];
+	size_t pat_len = strlen(pattern);
+	size_t n_possible_input = 127;
+	size_t n_state = strlen(pattern) + 1;
+	int dfa_table[n_state][n_possible_input];
+	construct_dfa(pattern, n_possible_input, n_state, dfa_table);
+	
 	Text_Pointer tp = {0};
-	tp.path = "shakespear-smol.txt";
-	tp.at_line = 1;	
 	tp.at_col = 1;
-
-    Min_String_Builder sb = {0};
-	if (!min_read_entire_file(tp.path, &sb)) return 1;
-
-	min_log(MIN_LOG, "Count: %d, Capacity: %d", sb.count, sb.capacity);
-	min_log(MIN_LOG, "Starting search...");
-
-	const char *pattern = "this";
-
-    dfa_string_matcher(pattern, sb, &tp);
-
-	min_free_sb(sb);
+	
+	for (int i = 2; i < argc; ++i) {
+		tp.path = argv[i];
+		Min_String_Builder sb = {0};
+		if (!min_read_entire_file(tp.path, &sb)) return 1;
+		dfa_string_matcher(pat_len, n_possible_input, dfa_table, sb, &tp);
+		/* naive_string_matching(sb, &tp, pattern);*/
+		min_free_sb(sb);
+	}
+	
 	return 0;
 }
